08b25b3db9c3f811c06d881eaac3a7466a6b2bbd
Junhyeok Soung <jhyeok1023@gmail.com>
2026-09-13 18:51:27 +0900

    Add swift-argument-parser

--- Package.swift
+++ Package.swift
@@ -5,15 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "trawl",
+    products: [
+        .executable(name: "trawl", targets: ["Trawl"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "trawl"
+            name: "Trawl",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
         ),
         .testTarget(
-            name: "trawlTests",
-            dependencies: ["trawl"]
+            name: "TrawlTests",
+            dependencies: ["Trawl"]
         ),
     ],
     swiftLanguageModes: [.v6]
---
+++ Sources/Trawl/Trawl.swift
@@ -0,0 +1,8 @@
+import ArgumentParser
+
+@main
+struct Trawl: ParsableCommand {
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
